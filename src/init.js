f86d5ef15f867498b55edb87beb45ae8a3453e4f
Vlad Babaev <vladbabaev11@gmail.com>
2023-01-08 23:53:11 +0500

    input field added

--- src/init.js
+++ src/init.js
@@ -1,4 +1,6 @@
 import i18next from 'i18next';
+import onChange from 'on-change';
+import * as yup from 'yup';
 import resources from '../locales/index.js';
 
 // Начальная функция
@@ -13,18 +15,72 @@ export default () => {
     },
   }).then(() => {
     const state = {
-      /* описание состояния */
+      inputValue: '',
+      feedsUrls: [],
+      lang: 'ru'
     };
 
     const feedForm = document.getElementById('rss-feed-form');
     const feedInput = document.getElementById('rss-feed-input');
+    const feedInputLabel = document.getElementById('input-label');
+    const inputValidationErrorDiv = document.getElementById('validation-error');
+    const formSubmitButton = document.getElementById('form-submit');
+    
 
-    console.log('feedForm: ', feedForm)
+    feedInputLabel.innerHTML = i18nextInstance.t('INPUT_LABEL');
+    formSubmitButton.innerHTML = i18nextInstance.t('SUBMIT');
+
+    const schema = yup.object().shape({
+      inputValue: yup.string()
+        .url('URL_VALIDATION_ERROR')
+        .test('value-duplicate', 'VALUE_DUPLICATE_ERROR', (value) => state.feedsUrls.every((source) => value !== source))
+        .required('REQUIRED_VALIDATION_ERROR'),
+    });
+
+    const watchedObject = onChange(state, (path, value, previousValue, applyData) => {
+      console.log('state: ', state);
+      // console.log('this:', this);
+      // console.log('path:', path);
+      // console.log('value:', value);
+      // console.log('previousValue:', previousValue);
+      // console.log('applyData:', applyData);
+
+      if (path === 'inputValue') {
+        schema.validate({ inputValue: value }).then(() => {
+          //--------------
+          feedInput.classList.add('is-valid');
+          feedInput.classList.remove('is-invalid');
+          inputValidationErrorDiv.classList.add('valid-feedback');
+          inputValidationErrorDiv.classList.remove('invalid-feedback');
+          inputValidationErrorDiv.innerHTML = '';
+          //-------------
+
+          state.feedsUrls.push(value);
+          state.inputValue = '';
+          feedForm.reset();
+        }).catch((err) => {
+          console.log('err.errors: ', err.errors);
+          // state.inputError = err.errors[0]
+          // inputValidationErrorDiv.innerHTML = err.errors[0];
+          inputValidationErrorDiv.innerHTML = i18nextInstance.t(err.errors[0])
+          feedInput.classList.add('is-invalid');
+          feedInput.classList.remove('is-valid');
+          inputValidationErrorDiv.classList.add('invalid-feedback');
+          inputValidationErrorDiv.classList.remove('valid-feedback');
+        });
+      }
+
+      if (path === 'inputError') {
+        // console.log('inputValidationErrorDiv.innerHTML: ', inputValidationErrorDiv.innerHTML)
+        inputValidationErrorDiv.innerHTML = value;
+      }
+    });
 
     feedForm.addEventListener('submit', (e) => {
       e.preventDefault();
-      console.log(feedInput.value);
-      feedForm.reset();
+      // console.log('event: ', e)
+      // console.log('feedInput.value: ', feedInput.value)
+      watchedObject.inputValue = feedInput.value;
     });
   });
 };
