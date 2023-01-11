97c7f9b87fef984e4eef7e44c4ef594b8f91dabb
Vlad Babaev <vladbabaev11@gmail.com>
2023-01-11 21:56:03 +0500

    add

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 /* eslint-disable global-require */
 /* eslint-disable no-underscore-dangle */
 
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
 import { fileURLToPath } from 'url';
@@ -13,15 +14,17 @@ const isProduction = process.env.NODE_ENV === 'production';
 const config = {
   entry: './src/index.js',
   output: {
-    filename: 'index.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer: {
-    static: path.resolve(__dirname, 'dist'),
-    port: 8080,
-    hot: true,
+    open: true,
+    host: 'localhost',
   },
-  plugins: [],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
   module: {
     rules: [
       {
@@ -62,3 +65,61 @@ export default () => {
   }
   return config;
 };
+
+/* eslint-disable no-underscore-dangle */
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+// import path from 'path';
+// import HtmlWebpackPlugin from 'html-webpack-plugin';
+// import WorkboxWebpackPlugin from 'workbox-webpack-plugin';
+// // import precss from 'precss';
+// // import autoprefixer from 'autoprefixer';
+// import { fileURLToPath } from 'url';
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // const path = require('path');
+// // const HtmlWebpackPlugin = require('html-webpack-plugin');
+// // const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
+
+// const isProduction = process.env.NODE_ENV === 'production';
+
+// const config = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   devServer: {
+//     open: true,
+//     host: 'localhost',
+//   },
+//   plugins: [
+//     new HtmlWebpackPlugin({
+//       template: 'index.html',
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//       {
+//         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+//         type: 'asset',
+//       },
+//     ],
+//   },
+// };
+
+// export default () => {
+//   if (isProduction) {
+//     config.mode = 'production';
+
+//     config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+//   } else {
+//     config.mode = 'development';
+//   }
+//   return config;
+// };
