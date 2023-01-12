8f79cfda64d9a15b9c10f991d3ab08d7a2a91886
Vlad Babaev <vladbabaev11@gmail.com>
2023-01-12 19:02:56 +0500

    optional chaining to error

--- src/rss-parser.js
+++ src/rss-parser.js
@@ -1,13 +1,18 @@
 const rssToDomNode = (content) => new window.DOMParser().parseFromString(content, 'text/xml');
 
 const parse = (data, feedId) => {
+  const parserError = data.querySelector('parsererror');
+
+  if (parserError) {
+    throw new Error('URL_NO_DATA_VALIDATION_ERROR');
+  }
   const title = data.querySelector('title')?.innerHTML;
   const description = data.querySelector('description')?.innerHTML;
   const link = data.querySelector('link')?.innerHTML;
   const id = btoa(unescape(encodeURIComponent(title))).slice(0, 19);
   const items = Array.prototype.map.call(data.querySelectorAll('item'), (item) => parse(item, id));
 
-  return items.length ? {
+  return items?.length ? {
     feed: {
       title, description, link, id,
     },
